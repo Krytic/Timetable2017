e67dc9e8bde309456c5561775ca0a0b66cb3ecf6
Scott Caldwell <scal353@aucklanduni.ac.nz>
2017-03-14 14:44:17 +1300

    Timetable function finished

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function start() {
   createTimetable();
+  setInterval(updateTime, 100);
+}
+
+function getDay() {
+  var date = new Date();
+  var weekday = new Array(7);
+  weekday[0] =  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  return weekday[date.getDay()];
+}
+
+function updateTime() {
+  var date = new Date();
+
+  document.getElementById("day").innerHTML = getDay();
+  document.getElementById("time").innerHTML = (date.getHours() % 13) + ":" + date.getMinutes() + (date.getHours() > 11 ? "PM" : "AM");
 }
 
 function createTimetable() {
@@ -18,7 +39,7 @@ function createTimetable() {
   var template = '<li class="times">%1%2%3</li>';
   if (!afternoon && morning) {
       for (var i = startTime; i < finishTime; i++) {
-        tt += template.replace('%1', i).replace('%2', "AM").replace("%3", getPeopleList (i + "AM"));
+        tt += template.replace('%1', i).replace('%2', "AM").replace("%3", getTimetableList (getDay(), i + "AM"));
       }
   } else if (morning && afternoon) {
     for (var i = startTime; i < 12; i++) {
@@ -30,16 +51,51 @@ function createTimetable() {
     }
   } else if (afternoon && !morning) {
     for (var i = startTime; i < finishTime; i++) {
-      tt += template.replace('%1', i).replace('%2', "PM").replace("%3", getPeopleList (i + "PM"));
+      tt += template.replace('%1', i).replace('%2', "PM").replace("%3", getTimetableList (getDay(), i + "PM"));
     }
   }
 
   document.getElementById('timetable').innerHTML = tt;
 }
 
-function getPeopleList (time) {
-  var timetable = JSON.parse(data);
-  var a = "";
+function getNames () {
+  return ["Sean", "Ryan", "Pierce", "Connor"];
+}
 
-  return a;
+function getTimetableList (day, time) {
+  names = getNames();
+  used_names = [];
+  var html = "<br />%1";
+  var times = data[day];
+  if (Object.keys(times).includes(time)) {
+    var classes = Object.keys(times[time]);
+    for (var i = 0; i < classes.length; i++) {
+      var className = classes[i]
+      html += "<br /><b>" + className + "</b>";
+      people = times[time][className]
+      for (var j = 0; j < people.length; j++) {
+        if (names.includes(people[j])) {
+          html += "<br />" + people[j];
+          used_names.push(people[j]);
+        }
+      }
+      html += "<br />";
+    }
+  }
+  var free = "";
+  var unused_names = [];
+  for (var i  = 0; i < names.length; i++) {
+    if (!used_names.includes(names[i])) {
+      unused_names.push(names[i]);
+    }
+  }
+  if (unused_names.length != 0) {
+    free += "<br /><b>Free</b>";
+    for (var i  = 0; i < unused_names.length; i++) {
+      free += "<br />" + unused_names[i];
+    }
+    free += "<br />";
+  }
+  html = html.replace("%1", free);
+  return html;
 }
